c74abd5f4f44ba788e5222024ccf5be508b8aaf0
thaithuytb <ngoquangthai29112001@gmail.com>
2022-05-04 21:55:49 +0700

    editing in user.controller

--- libs/users/src/lib/users.service.ts
+++ libs/users/src/lib/users.service.ts
@@ -9,31 +9,44 @@ export class UsersService {
   readonly users: User[] = [...dataUser];
 
   findUserByUsername(username: string): User {
-      return this.users.find((user) => user.username === username);
+    return this.users.find((user) => user.username === username);
   }
 
-  deleteUserByUsername(username: string): User {
+  deleteUserByUsername(username: string): User | null {
     const checkUser = this.users.find((user) => user.username === username);
     if (checkUser) {
       this.users.splice(checkUser.id, 1);
       return checkUser;
     }
+    return null;
   }
 
   findUserById(id: number): User {
     return this.users.find((user) => user.id === id);
   }
 
-  createUser( createUserDto:CreateUserDto): User | null {
-    const checkUser = this.users.find((user) => user.username === createUserDto.username);
+  createUser(createUserDto: CreateUserDto): User | null {
+    const checkUser = this.users.find(
+      (user) => user.username === createUserDto.username
+    );
     if (!checkUser) {
       const newUser = new UserEntity();
       newUser.username = createUserDto.username;
       newUser.password = createUserDto.password;
       this.users.push({
         ...newUser,
-      })
+      });
       return newUser;
+    }
+    return null;
+  }
+
+  updateUser(username: string, updateUser: CreateUserDto): User | null {
+    const checkUser = this.users.find((user) => user.username === username);
+    if (checkUser) {
+      const updateUserIndex = this.users.indexOf(checkUser);
+      this.users[updateUserIndex] = updateUser as User;
+      return updateUser as User;
     }
     return null;
   }
